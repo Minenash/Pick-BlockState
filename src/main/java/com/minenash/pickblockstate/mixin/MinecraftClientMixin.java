1d32df194b5cca27a39bcf2c6d54ba74c4a8a616
Jakob <minenash@protonmail.com>
2023-12-14 02:38:36 -0500

    Updated to 1.19.3/4

--- src/main/java/com/minenash/pickblockstate/mixin/MinecraftClientMixin.java
+++ src/main/java/com/minenash/pickblockstate/mixin/MinecraftClientMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,8 +24,8 @@ public class MinecraftClientMixin {
 
 	@Shadow public ClientPlayerEntity player;
 
-	@Redirect(method = "doItemPick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getPickStack(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Lnet/minecraft/item/ItemStack;"))
-	public ItemStack getPickStack(Block block, BlockView world, BlockPos pos, BlockState state) {
+	@Redirect(method = "doItemPick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getPickStack(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Lnet/minecraft/item/ItemStack;"))
+	public ItemStack getPickStack(Block block, WorldView world, BlockPos pos, BlockState state) {
 		ItemStack stack = block.getPickStack(world, pos, state);
 
 		if (!player.getAbilities().creativeMode || !Screen.hasControlDown() || state.hasBlockEntity())
